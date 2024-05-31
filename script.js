72b4064fccadee3d8eb84630e9ca006b8142c8bc
Sasha Waszaj <swaszajf@gmail.com>
2024-05-31 11:07:41 -0400

    update

---
+++ script.js
@@ -0,0 +1,21 @@
+var count=0;
+var countElement = document.querySelector('#count')
+
+function add1(){
+    count++;
+    countElement.innerText = count + "like(s)";
+}
+var count2=0;
+var count2Element = document.querySelector('#count2')
+
+function add12(){
+    count2++;
+    count2Element.innerText = count2 + "like(s)";
+}
+var count3=0;
+var count3Element = document.querySelector('#count3')
+
+function add13(){
+    count3++;
+    count3Element.innerText = count3 + "like(s)";
+}
